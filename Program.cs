78f484fa73f2306bb8b814f4ecb2451ed3b90871
isabela-cortorreal <78940846+isabela-cortorreal@users.noreply.github.com>
2021-02-11 22:07:39 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char abc;
+
+            for (abc = 'Z'; abc >= 'A'; abc--)
+            {
+                Console.WriteLine("{0}", abc);
+            }
+            Console.ReadKey();
+        }
+    }
+}
