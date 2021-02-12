8db93e458502c0ff0adbc3dd1ee7d249fef4ad4e
isabela-cortorreal <78940846+isabela-cortorreal@users.noreply.github.com>
2021-02-11 23:32:02 -0400

    Add files via upload

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1;
+
+            do
+            {
+                Console.WriteLine("{0}", numero);
+                numero++;
+            } while (numero != 11);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeros 10 números pares a partir del producto de (10 x 10)");
+
+            for (int i = 100; i <= 118; i = i + 2)
+            {
+                Console.WriteLine(i + "\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
